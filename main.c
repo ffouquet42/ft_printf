3a56852b2b5028837af12668bf253a86bd81b7ec
ffouquet42 <ffouquet42@gmail.com>
2022-05-18 06:58:26 +0200

    cleaning of my own tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 05:32:39 by fllanet           #+#    #+#             */
-/*   Updated: 2022/05/17 11:53:22 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/05/18 06:55:22 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,67 @@
 int	main(void)
 {
 	// Test without percentage
+	ft_printf("\n=== Test without percentage ===\n\n");
+
 	ft_printf("Hello\n");
 
+	ft_printf("\n-------------------------------\n");
+
 	// Test %c
+	ft_printf("\n=== Test %%c ===\n\n");
+
 	ft_printf("Hello %c\n", 'a');
 	ft_printf("Hello %c%c%c\n", 'a', 'b', 'c');
 	
+	ft_printf("\n-------------------------------\n");
+	
 	// Test %s
+	ft_printf("\n=== Test %%s ===\n\n");
+
 	ft_printf("Hello %s\n", "soum");
+	
+	ft_printf("\n-------------------------------\n");
 
 	// Test %d and %i
+	ft_printf("\n=== Test %%d and %%i ===\n\n");
+
 	ft_printf("Hello %i world %d\n", -123, 789);
+	printf("Hello %i world %d\n", -123, 789);
+	
+	ft_printf("\n-------------------------------\n");
 
 	// Test %u
+	ft_printf("\n=== Test %%u ===\n\n");
+
 	ft_printf("Hello %u world %u\n", 123, 789);
+	printf("Hello %u world %u\n", 123, 789);
+	ft_printf("Hello %u world %u\n", -123, 789);
+	printf("Hello %u world %u\n", -123, 789);
+	
+	ft_printf("\n-------------------------------\n");
 	
 	// Test %x and %X
+	ft_printf("\n=== Test %%x and %%X ===\n\n");
+	
 	ft_printf("Hello %x world %X\n", 123456, -9807);
-	printf("Hello %x world %X\n", 123456, -9807); // Neg ???
+	printf("Hello %x world %X\n", 123456, -9807);
+	
+	ft_printf("\n-------------------------------\n");
 	
 	// Test %%
+	ft_printf("\n=== Test %%%% ===\n\n");
+
 	ft_printf("Hello %%\n");
+	
+	ft_printf("\n-------------------------------\n");
 
 	// Test unvalid char
+	ft_printf("\n=== Test unvalid char ===\n\n");
+
 	ft_printf("Hello %y\n");
 	printf("Hello %y\n");
+	
+	ft_printf("\n-------------------------------\n");
 	
 	return (0);
 }
